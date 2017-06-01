c7e22bd876f6ad1068254840f08a114c22e81d23
huangmeng <huangmeng@kgc.com>
2017-06-01 23:38:06 +0800

    1、添加订单维护方法

--- shop/src/main/java/com/gaoling/shop/order/dao/OrderDao.java
+++ shop/src/main/java/com/gaoling/shop/order/dao/OrderDao.java
@@ -1,14 +1,18 @@
 package com.gaoling.shop.order.dao;
 
 import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
-import com.gaoling.shop.user.pojo.Address;
+import com.gaoling.shop.order.pojo.Order;
 
 @Repository
 public interface OrderDao {
 
-	List<Address> queryAllOrder();
+	List<Order> queryOrders(@Param("param")Map<Object,Object> param);
+	void addOrder(Order order);
+	void updateOrder(Order order);
 	
 }
--- shop/src/main/java/com/gaoling/shop/order/service/OrderService.java
+++ shop/src/main/java/com/gaoling/shop/order/service/OrderService.java
@@ -1,15 +1,40 @@
 package com.gaoling.shop.order.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.gaoling.shop.user.dao.UserDao;
+import com.gaoling.shop.order.dao.OrderDao;
+import com.gaoling.shop.order.pojo.Order;
+import com.gaoling.shop.system.pojo.Result;
+import com.gaoling.shop.system.service.CommonService;
 
 @Service
-public class OrderService {
+public class OrderService extends CommonService{
 
 	@Autowired
-	private UserDao orderDao;
+	private OrderDao orderDao;
+	
+	//用户下单
+	public Result newOrder(){
+		
+	}
 
+	//查询订单信息
+	public List<Order> queryOrders(Map<Object,Object> param){
+		return orderDao.queryOrders(param);
+	}
+	
+	//保存订单信息
+	public void addOrder(Order order){
+		orderDao.addOrder(order);
+	}
+	
+	//修改订单信息
+	public void updateOrder(Order order){
+		orderDao.updateOrder(order);
+	}
 	
 }
