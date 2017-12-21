46f4e0ab1738a0c92048a39d7dcc41b2da920b17
黄盟 <huangmeng@haotang365.com.cn>
2017-12-21 20:11:55 +0800

    1、好吧，忘记配置@ResponseBody了- -！！

--- shop/src/main/java/com/gaoling/shop/pay/action/CashExchangeController.java
+++ shop/src/main/java/com/gaoling/shop/pay/action/CashExchangeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gaoling.shop.common.AppConstant;
 import com.gaoling.shop.pay.service.CashExchangeLogService;
@@ -17,6 +18,7 @@ public class CashExchangeController {
 	private CashExchangeLogService cashExchangeLogService;
 	
 	//我的部落币详情
+	@ResponseBody
 	@RequestMapping("/myCoin")
 	public Result orderDetail(@RequestParam(required=false)String uuid){
 		Result result=null;
@@ -30,6 +32,7 @@ public class CashExchangeController {
 	}
 	
 	//部落币提现
+	@ResponseBody
 	@RequestMapping("/exchange")
 	public Result coinCashExchange(@RequestParam(required=false)String uuid,@RequestParam(defaultValue="0")Integer coin){
 		Result result=null;
@@ -43,6 +46,7 @@ public class CashExchangeController {
 	}
 	
 	//加载部落币提现记录
+	@ResponseBody
 	@RequestMapping("/exchangeHistory")
 	public Result exchangeHistory(@RequestParam(required=false)String uuid){
 		Result result=null;
@@ -56,6 +60,7 @@ public class CashExchangeController {
 	}
 	
 	//定时执行用户提现申请
+	@ResponseBody
 	@RequestMapping("/doExchange")
 	public Result executeCashExchangeOntime(@RequestParam(required=false)String uuid){
 		Result result=null;
